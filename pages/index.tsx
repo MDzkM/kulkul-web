6f27cb5c6cbe486e519fbde4aa51690da66d3220
Muhammad Dzikra Muzaki <mdzkm17@gmail.com>
2022-08-16 18:14:47 +0700

    Change contents of menus

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Typewriter from "typewriter-effect"
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -14,40 +16,46 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="">KULKUL</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by registering your{' '}
+            <Typewriter
+              options={{
+                loop: true,
+                autoStart: true,
+                strings: ["fridge", "food", "recipe"],
+              }}
+            />
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="" className={styles.card}>
+            <h2>Fridges &rarr;</h2>
+            <p>View all your fridges in one place, including their contents.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="" className={styles.card}>
+            <h2>Login &rarr;</h2>
+            <p>Hop into your account before managing your provisions!</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href=""
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Recipes &rarr;</h2>
+            <p>Explore various recipes previously acquired and expand it.</p>
+          </a>
+
+          <a
+            href=""
+            className={styles.card}
+          >
+            <h2>Expiry &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Keep track of expiration dates and be notified for close to expiring items.
             </p>
           </a>
         </div>
@@ -55,14 +63,11 @@ const Home: NextPage = () => {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://mdzkm.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Copyright © 2022 mdzkm. All Rights Reserved
         </a>
       </footer>
     </div>
